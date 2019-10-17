db55ee49973df208e6b913a3fce63380673b68a8
Margot Ruaud <margotruaud@gmail.com>
2019-10-17 21:33:45 +0200

    almost done

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -27,25 +27,84 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+memoryGame.shuffleCards();
+
 document.addEventListener("DOMContentLoaded", function(event) { 
   let html = '';
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
-    html += `<div class="back" name="${pic.img}"></div>`;
-    html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
+    html += `  <div class="back" name="${pic.img}"></div>`;
+    html += `  <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
     html += `</div>`;
   });
+
+
+  function showCard(card){
+      card.classList.remove('back');
+      card.classList.add('front');
+      card.nextElementSibling.classList.remove('front');
+      card.nextElementSibling.classList.add('back');
+
+
+  }
+
+  function hideCard(card){
+    console.log("coucoucoucou");
+    console.log(card);
+    card.classList.remove('front');
+    card.classList.add('back');
+    card.nextElementSibling.classList.remove('back');
+    card.nextElementSibling.classList.add('front');
+  }
+
+  function showCardForever(e){
+    card.classList.remove('back');
+    card.classList.add('front');
+    card.nextElementSibling.classList.remove('front');
+    card.nextElementSibling.classList.add('back');
+  }
 
   // Add all the divs to the HTML
   document.querySelector('#memory_board').innerHTML = html;
 
+  //while(memoryGame.isFinished === false){
+
+  var card1="";
+  var card2="";
+  var compt=0;
   // Bind the click event of each element to a function
   document.querySelectorAll('.back').forEach( card => {
     card.onclick = function() {
       // TODO: write some code here
+      console.log("eis");
+      console.log(card);
+      showCard(card);
+      console.log("eisnow");
+      console.log(card);
+      setTimeout(function(){
+        console.log("haloa");
+        hideCard(card);
+      },1000);
+      
       console.log('Card clicked: ', card);
+      if (compt===0){
+        card1=card.getAttribute("name");
+        compt=1;
+      }
+      else{
+        card2=card.getAttribute("name");
+        let pc=+document.querySelector("#pairs_clicked").textContent;
+        document.querySelector("#pairs_clicked").innerHTML= (pc+1).toString();  
+
+        if (memoryGame.checkIfPair(card1,card2)){
+          let pg=+document.querySelector("#pairs_guessed").textContent;
+          document.querySelector("#pairs_guessed").innerHTML= (pg+1).toString();
+        }
+        compt=0;
+      }
+      
     };
   });
 });
-
+//}
 
