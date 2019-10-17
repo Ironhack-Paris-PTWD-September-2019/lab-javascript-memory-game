dc089ec45e9e2fe8175d327592a36dd03be03573
Sandrine8304 <sandrine.auberval@gmail.com>
2019-10-17 21:36:43 +0200

    1st commit

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,28 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
+
+  shuffleCards() {
+    for (let i = this.cards.length; i > 1; i--) {
+      var randomNbCard = Math.floor(Math.random() * i);
+      cards[i] = cards[randomNbCard];
+    }
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   isFinished() {}
 }
