d048bbe803c0f3731c516e7e0a9c7a73cc38bc71
Tracy Bonjour <tracybonjour@MacBook-Air-de-Tracy.local>
2019-10-17 21:37:35 +0200

    Iteration 3 ok rest done

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,37 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []; 
+    this.pairsClicked = 0; 
+    this.pairsGuessed = 0; 
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    for (let i = this.cards.length-1; i >= 1; i--) {
+      let j = Math.floor(Math.random()*i);
+      let temp = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = temp;
+    }
+  }
+
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isFinished() {
+    // let result = false;
+    // if (this.pairsGuessed === this.cards.length/2) {
+    //   result = true;
+    // }
+    // return result;
+    // Expression ternaire
+    return this.pairsGuessed === this.cards.length/2? true:false;
+  }
 }
