65a26e1892bac7e4b498eac7acc5851761108541
Enelyah <ms.jen.christine@gmail.com>
2019-10-17 21:42:28 +0200

    commit raté - jenny

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,39 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
-}
+  // shuffleCards ne marche qu'après le first click
+  shuffleCards() {
+    var currentIndex = this.cards.length;
+    var temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = this[currentIndex];
+      this.cards[currentIndex] = this.cards[randomIndex];
+      this.cards[randomIndex] = temporaryValue;
+    }
+    return this.cards;
+    
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked+=1;
+    if (card1===card2){
+      this.pairsGuessed+=1;
+      return true;
+    } else {
+      return false;
+    };
+  }
+  isFinished() {
+   return this.pairsGuessed>=this.cards.length/2
+    }
+  }
