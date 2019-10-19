c762f4851c2fb1be46abbe6373f08a55c918fe96
Sadigova Farida <Farida@macbook-air-de-sadigova.home>
2019-10-20 00:49:38 +0200

    Farida Fisher Yates shuffle algorithm

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,28 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    for (let i = this.cards.length - 1; i >= 1; i--) {
+      let j = Math.floor(Math.random() * i);
+      let temp = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = temp;
+    }
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    return this.pairsGuessed >= (this.cards.length / 2);
+  }
 }
